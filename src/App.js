35ac7267344a4b9f2a9cb14841fd59eaa72acdc7
PReichetanz <paul.reichetanz@gmx.de>
2022-02-08 10:50:43 +0100

    Create basic Room component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
-import "./App.css";
+import Room from './Room.js';
+import './App.css';
 
 export default function App() {
-  return "Hello world!";
+  return (
+    <main className="App">
+      <Room text="Küche" isClean />
+      <Room text="Bad" />
+      <Room text="Wohnzimmer" />
+    </main>
+  );
 }
