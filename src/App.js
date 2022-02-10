471da40297ef1f411089781e7fb989daaeeb4bf6
PReichetanz <paul.reichetanz@gmx.de>
2022-02-10 15:16:15 +0100

    Add localStorage with useEffect()

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import Header from './Header';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import produce from 'immer';
 import './App.css';
 
 export default function App() {
-  const [rooms, setRooms] = useState([
-    {
-      title: 'Küche',
-    },
-    {
-      title: 'Wohnzimmer',
-    },
-    {
-      title: 'Bad',
-    },
-  ]);
+  const [rooms, setRooms] = useState(
+    loadFromLocal('rooms') ?? [
+      {
+        title: 'Küche',
+      },
+      {
+        title: 'Wohnzimmer',
+      },
+      {
+        title: 'Bad',
+      },
+    ]
+  );
+
+  useEffect(() => {
+    saveToLocal('rooms', rooms);
+  }, [rooms]);
 
   return (
     <main className="App">
@@ -26,11 +32,24 @@ export default function App() {
       ))}
     </main>
   );
+
   function toggleCleaningStatus(index) {
     // with immerjs:
     const newState = produce(rooms, (draft) => {
       draft[index].isClean = !draft[index].isClean;
     });
     setRooms(newState);
+  }
+
+  function loadFromLocal(key) {
+    try {
+      return JSON.parse(localStorage.getItem(key));
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  function saveToLocal(key, data) {
+    localStorage.setItem(key, JSON.stringify(data));
   }
 }
